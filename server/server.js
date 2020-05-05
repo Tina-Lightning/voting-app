1ff72c2341e7df3beeb883b16db06a47d7986448
Chrissy <c.atherholt@gmail.com>
2020-05-05 16:54:03 -0400

    added login and register

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const db = require("./models");
 const handle = require("./handlers");
+const routes = require("./routes");
 
 const app = express();
 const port = process.env.PORT;
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.json({hello: "world"}));
+app.use("/api/auth", routes.auth)
 
 //create an error handler
 app.use(handle.notFound);
